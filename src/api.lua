5175de069b873e6bd4370092e56b89f1856a4815
Otto Pattemore <otto.pattemore@gmail.com>
2022-06-03 09:25:10 +1000

    🔧 More code cleanup

--- src/api.lua
+++ src/api.lua
@@ -1,18 +1,20 @@
-function register_leaf(name, title, color, dye, include_light, include_dark)
-
-	-- Normal
+local function register_leaf_node(name, title, texture_mod)
     minetest.register_node("colored_leaves:"..name, {
     	description = title.." Tree Leaves",
     	drawtype = "allfaces_optional",
     	waving = 1,
-    	tiles = {"default_leaves.png^[colorize:"..color..":100"},
-    	special_tiles = {"default_leaves.png^[colorize:"..color..":100"},
+    	tiles = {"default_leaves.png"..texture_mod},
+    	special_tiles = {"default_leaves.png"..texture_mod},
     	paramtype = "light",
     	is_ground_content = false,
     	groups = {snappy = 3, leafdecay = 3, flammable = 2, leaves = 1},
     	sounds = default.node_sound_leaves_defaults(),
     	after_place_node = after_place_leaves,
     })
+end
+function register_leaf(name, title, color, dye, include_light, include_dark)
+    -- Normal
+    register_leaf_node(name, title, "^[colorize:"..color..":100")
     minetest.register_craft({
     	type = "shapeless",
     	output = "colored_leaves:"..name,
@@ -22,20 +24,9 @@ function register_leaf(name, title, color, dye, include_light, include_dark)
     	},
 	})
 
-	-- Dark
+
 	if ( include_dark == nil ) or ( include_dark == true ) then
-    	minetest.register_node("colored_leaves:"..name.."_dark", {
-    		description = "Dark "..title.." Tree Leaves",
-    		drawtype = "allfaces_optional",
-    		waving = 1,
-    		tiles = {"default_leaves.png^[multiply:"..color},
-    		special_tiles = {"default_leaves.png^[multiply:"..color},
-    		paramtype = "light",
-    		is_ground_content = false,
-    		groups = {snappy = 3, leafdecay = 3, flammable = 2, leaves = 1},
-    		sounds = default.node_sound_leaves_defaults(),
-    		after_place_node = after_place_leaves,
-    	})
+        register_leaf_node(name.."_dark", "Dark " .. title, "^[multiply:"..color)
     	minetest.register_craft({
     		type = "shapeless",
     		output = "colored_leaves:"..name.."_dark",
@@ -45,23 +36,10 @@ function register_leaf(name, title, color, dye, include_light, include_dark)
     		    dye,
     		},
 		})
-	end
+    end
 
-
-	-- Light
 	if ( include_light == nil ) or ( include_light == true ) then
-    	minetest.register_node("colored_leaves:"..name.."_light", {
-    		description = "Light "..title.." Tree Leaves",
-    		drawtype = "allfaces_optional",
-    		waving = 1,
-    		tiles = {"default_leaves.png^[colorize:"..color..":150"},
-    		special_tiles = {"default_leaves.png^[colorize:"..color..":150"},
-    		paramtype = "light",
-    		is_ground_content = false,
-    		groups = {snappy = 3, leafdecay = 3, flammable = 2, leaves = 1},
-    		sounds = default.node_sound_leaves_defaults(),
-    		after_place_node = after_place_leaves,
-    	})
+        register_leaf_node(name.."_light", "Light " .. title, "^[colorize:"..color..":150")
     	minetest.register_craft({
     		type = "shapeless",
     		output = "colored_leaves:"..name.."_light",
@@ -71,5 +49,5 @@ function register_leaf(name, title, color, dye, include_light, include_dark)
     		    dye,
     		},
 		})
-	end
+    end
 end
